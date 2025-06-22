1a571bb97757bf1d19686bf3fb9e39cd09287464
msqr1 <134968689+msqr1@users.noreply.github.com>
2025-06-21 21:31:52 -0700

    SSE trial, -10.62ms/decode but unacceptable size increase

--- libde265.bash
+++ libde265.bash
@@ -2,12 +2,13 @@
 
 source emsdk/emsdk_env.sh &&
 
-LIBDE265=$(realpath libde265)
+rootDir=$(realpath .)
 
 cd /tmp/libde265 &&
-./autogen.sh
-compileFlags="-Oz -flto -fno-exceptions -DNDEBUG -sSUPPORT_LONGJMP=0 -mmutable-globals -mreference-types -mtail-call -mextended-const -msimd128"
+git apply "${rootDir}/libde265.patch" &&
+./autogen.sh &&
+compileFlags="-Oz -flto -fno-exceptions -DNDEBUG -sSUPPORT_LONGJMP=0 -mmutable-globals -mreference-types -mtail-call -mextended-const -msimd128 -msse4.1"
 linkFlags="${compileFlags} -sFILESYSTEM=0 -sWASMFS"
-CFLAGS="${compileFlags}" CXXFLAGS="${compileFlags}" LDFLAGS="${linkFlags}"  emconfigure ./configure --enable-static --disable-shared --disable-dec265 --disable-sherlock265 --disable-sse --disable-arm --disable-dependency-tracking --disable-log-error --prefix="${LIBDE265}" &&
+CFLAGS="${compileFlags}" CXXFLAGS="${compileFlags}" LDFLAGS="${linkFlags}"  emconfigure ./configure --enable-static --disable-shared --disable-dec265 --disable-sherlock265 --disable-arm --disable-dependency-tracking --disable-log-error --prefix="${rootDir}/libde265" &&
 emmake make -j"$(nproc)" &&
 make install
